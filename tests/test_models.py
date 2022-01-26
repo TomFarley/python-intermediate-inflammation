824f6e1a43eddbeeafcdc1f69b5e5d084288e1f1
Tom Farley <tom.farley@ukaea.uk>
2022-01-26 16:37:19 +0000

    Add test

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,8 @@
 import numpy as np
 import numpy.testing as npt
 import pytest
+import hypothesis
+from hypothesis import strategies
 
 
 def test_daily_min_string():
@@ -11,6 +13,29 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+        ([], [])
+    ])
+def test_daily_mean(test, expected):
+    """Test that mean function works for zeros and positive integers."""
+    from inflammation.models import daily_mean
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
+@hypothesis.given(strategies.lists(strategies.integers()))
+def test_daily_min(test, expected):
+    """Test that mean function works for zeros and positive integers."""
+    from inflammation.models import daily_min
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
