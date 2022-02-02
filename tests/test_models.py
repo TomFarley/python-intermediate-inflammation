0e93285920b1e2cd2c164039ba60924952093568
Tom Farley <tom.farley@ukaea.uk>
2022-02-02 14:52:11 +0000

    Add and test patient_normalise

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,13 +29,13 @@ def test_daily_mean(test, expected):
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
-@hypothesis.given(strategies.lists(strategies.integers()))
-def test_daily_min(test, expected):
-    """Test that mean function works for zeros and positive integers."""
-    from inflammation.models import daily_min
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+# @hypothesis.given(strategies.lists(strategies.integers()))
+# def test_daily_min(test, expected):
+#     """Test that mean function works for zeros and positive integers."""
+#     from inflammation.models import daily_min
+#
+#     # Need to use Numpy testing functions to compare arrays
+#     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -86,5 +86,55 @@ def test_daily_min():
     test_result = np.array([-4, -6, 2])
 
     npt.assert_array_equal(daily_min(test_input), test_result)
+
+@pytest.mark.parametrize(
+    "test, expected, raises",
+    [
+        (
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            None,
+        ),
+        (
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None,
+        ),
+        (
+            [[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[0, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None,
+        ),
+        (
+            [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]],
+            None,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        ),
+        (
+            [[float('nan'), float('nan'), float('nan')], [4, 5, 6], [7, 8, 9]],
+            [[0, 0, 0], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        ),
+        (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            ValueError,
+        ),
+    ])
+def test_patient_normalise(test, expected, raises):
+    """Test normalisation works for arrays of one and positive integers.
+       Assumption that test accuracy of two decimal places is sufficient."""
+    from inflammation.models import patient_normalise
+    if raises:
+        with pytest.raises(raises):
+            npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+
 
 # TODO(lesson-robust) Implement tests for the other statistical functions
